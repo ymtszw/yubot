8e1468dfde9a4557e8e87aa29844f71428663a1d
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2017-02-08 12:37:53 +0900

    Remove generated route

--- web/controller/hello.ex
+++ web/controller/hello.ex
@@ -1,8 +1,3 @@
 defmodule Yubot.Controller.Hello do
   use SolomonLib.Controller
-
-  def hello(conn) do
-    Yubot.Gettext.put_locale(conn.request.query_params["locale"] || "en")
-    render(conn, 200, "hello", [gear_name: :yubot])
-  end
 end
--- web/router.ex
+++ web/router.ex
@@ -2,6 +2,4 @@ defmodule Yubot.Router do
   use SolomonLib.Router
 
   static_prefix "/static"
-
-  get "/hello", Hello, :hello
 end
