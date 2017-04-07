e32e37c6c08376b0798bd8722ea0ef55bdd070bf
ymtszw <ymtszw@gmail.com>
2017-04-08 03:14:50 +0900

    Compile Cleanup module only when in blackbox_test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -12,22 +12,24 @@ defmodule Socket do
   use SolomonLib.Test.WebsocketClient
 end
 
-defmodule Yubot.Blackbox.Cleanup do
-  alias Yubot.Model.{Poll, Action, Authentication}
+if SolomonLib.Test.Config.blackbox_test?() do
+  defmodule Yubot.Blackbox.Cleanup do
+    alias Yubot.Model.{Poll, Action, Authentication}
 
-  @test_group_id (case SolomonLib.Test.Config.test_mode() do
-    :blackbox_prod -> raise("Not ready!")
-    :blackbox_dev -> "g_4xxYWNkn"
-    :blackbox_local -> "g_zCGtN44K"
-  end)
+    @test_group_id (case SolomonLib.Test.Config.test_mode() do
+      :blackbox_prod -> raise("Not ready!")
+      :blackbox_dev -> "g_4xxYWNkn"
+      :blackbox_local -> "g_zCGtN44K"
+    end)
 
-  def run() do
-    root_key = SolomonLib.Test.Config.blackbox_test_secret()["dodai_root_key"]
-    {:ok, polls} = Poll.retrieve_list(%{}, root_key, @test_group_id)
-    Enum.each(polls, fn %Poll{_id: poll_id} -> Poll.delete(poll_id, nil, root_key, @test_group_id) end)
-    {:ok, actions} = Action.retrieve_list(%{}, root_key, @test_group_id)
-    Enum.each(actions, fn %Action{_id: action_id} -> Action.delete(action_id, nil, root_key, @test_group_id) end)
-    {:ok, auths} = Authentication.retrieve_list(%{}, root_key, @test_group_id)
-    Enum.each(auths, fn %Authentication{_id: auth_id} -> Authentication.delete(auth_id, nil, root_key, @test_group_id) end)
+    def run() do
+      root_key = SolomonLib.Test.Config.blackbox_test_secret()["dodai_root_key"]
+      {:ok, polls} = Poll.retrieve_list(%{}, root_key, @test_group_id)
+      Enum.each(polls, fn %Poll{_id: poll_id} -> Poll.delete(poll_id, nil, root_key, @test_group_id) end)
+      {:ok, actions} = Action.retrieve_list(%{}, root_key, @test_group_id)
+      Enum.each(actions, fn %Action{_id: action_id} -> Action.delete(action_id, nil, root_key, @test_group_id) end)
+      {:ok, auths} = Authentication.retrieve_list(%{}, root_key, @test_group_id)
+      Enum.each(auths, fn %Authentication{_id: auth_id} -> Authentication.delete(auth_id, nil, root_key, @test_group_id) end)
+    end
   end
 end
