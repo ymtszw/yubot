2da4ab7fae456bf72d40afab0c54e51cc7737dfb
ymtszw <ymtszw@gmail.com>
2017-05-03 01:13:55 +0900

    Add label and type to Action

--- web/model/action.ex
+++ web/model/action.ex
@@ -1,12 +1,25 @@
 use Croma
 
 defmodule Yubot.Model.Action do
+  @moduledoc """
+  HTTP action object.
+  """
+
   alias Yubot.Model.Authentication
 
+  defmodule Type do
+    use Croma.SubtypeOfAtom, values: [
+      :http,
+      :hipchat,
+    ], default: :http
+  end
+
   use SolomonAcs.Dodai.Model.Datastore, data_fields: [
+    label: Croma.TypeGen.nilable(Croma.String),
     method: SolomonLib.Http.Method,
     url: SolomonLib.Url,
     auth: Croma.TypeGen.nilable(Authentication.Id),
     body_template: Yubot.StringTemplate,
+    type: Type,
   ]
 end
