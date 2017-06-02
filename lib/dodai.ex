a4857c4d1fee72ee81cbcb69ce0cb8c0de8f3458
ymtszw <ymtszw@gmail.com>
2017-06-03 00:53:18 +0900

    Add runtime APIs for asset upload task

--- lib/dodai.ex
+++ lib/dodai.ex
@@ -9,11 +9,19 @@ defmodule Yubot.Dodai do
     :dev -> "g_4xxYWNkn"
     _local -> "g_zCGtN44K"
   end)
+  @app_id "a_Eih41ySz"
 
-  use SolomonAcs.Dodai.GearModule, app_id: "a_Eih41ySz", default_group_id: @default_group_id
+  use SolomonAcs.Dodai.GearModule, app_id: @app_id, default_group_id: @default_group_id
 
   def app_key(),  do: Yubot.get_env("dodai_app_key")
   def root_key(), do: Yubot.get_env("dodai_root_key")
 
   def test_group_id(), do: @test_group_id
+
+  def app_id("prod"), do: raise("not ready!")
+  def app_id(_dev_or_local), do: "a_Eih41ySz"
+
+  def group_id("prod"), do: raise("not ready!")
+  def group_id("dev"), do: "g_9eTTqdNt"
+  def group_id(_local), do: "g_MbhtDhFm"
 end
