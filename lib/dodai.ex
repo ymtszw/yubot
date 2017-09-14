10348ad55f34e8f184632019928a97e49cb1cc52
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2017-09-14 18:25:45 +0900

    Remove obsolete modules; add default_client_config

--- lib/dodai.ex
+++ lib/dodai.ex
@@ -4,19 +4,19 @@ defmodule Yubot.Dodai do
     :dev -> "g_9eTTqdNt"
     _local -> "g_MbhtDhFm"
   end)
+  use SolomonAcs.Dodai.GearModule,
+    app_id: "a_Eih41ySz",
+    default_group_id: @default_group_id,
+    default_client_config: %{recv_timeout: 10_000}
+
+  def app_key(),  do: Yubot.get_env("dodai_app_key")
+  def root_key(), do: Yubot.get_env("dodai_root_key")
+
   @test_group_id (case SolomonLib.Env.compile_env() do
     :prod -> raise("Not ready!")
     :dev -> "g_4xxYWNkn"
     _local -> "g_zCGtN44K"
   end)
-  @app_id "a_Eih41ySz"
-
-  use SolomonAcs.Dodai.GearModule, app_id: @app_id, default_group_id: @default_group_id
-  import Croma.TypeGen, only: [nilable: 1]
-
-  def app_key(),  do: Yubot.get_env("dodai_app_key")
-  def root_key(), do: Yubot.get_env("dodai_root_key")
-
   def test_group_id(), do: @test_group_id
 
   def app_id("prod"), do: raise("not ready!")
@@ -25,22 +25,4 @@ defmodule Yubot.Dodai do
   def group_id("prod"), do: raise("not ready!")
   def group_id("dev"), do: "g_9eTTqdNt"
   def group_id(_local), do: "g_MbhtDhFm"
-
-  defmodule Session do
-    use Croma.Struct, fields: [
-      key:                 nilable(Croma.String),
-      expires_at:          nilable(SolomonLib.Time),
-      password_set_at:     SolomonLib.Time,
-      password_expires_at: nilable(SolomonLib.Time),
-    ], accept_case: :lower_camel, recursive_new?: true
-  end
-
-  defmodule AppUsageRuleOfUser do
-    use Croma.Struct, fields: [
-      app_id:     Dodai.AppId,
-      usage_rule: Croma.String,
-      starts_at:  nilable(SolomonLib.Time),
-      ends_at:    nilable(SolomonLib.Time),
-    ], accept_case: :lower_camel, recursive_new: true
-  end
 end
