ab4431d63359fc7c00e75f7223cba0b15a961419
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-01-10 17:53:26 +0900

    [master] Update Gettext => SolomonLib.Gettext

--- lib/gettext.ex
+++ lib/gettext.ex
@@ -1,7 +1,7 @@
 use Croma
 
 defmodule Yubot.Gettext do
-  use Gettext, otp_app: :yubot
+  use SolomonLib.Gettext, otp_app: :yubot
 
   defun put_locale(locale :: v[String.t]) :: nil do
     Gettext.put_locale(__MODULE__, locale)
